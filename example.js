bbf06755b690443f2f05cd3f27d4aa930781da5f
Lerisson Freitas - lff3 <lff3@cin.ufpe.br>
2019-03-13 17:49:08 -0300

    Update example.js

--- example.js
+++ example.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Nivio!');
+  res.end('Hello Lucas!');
 }).listen(8080);
 
